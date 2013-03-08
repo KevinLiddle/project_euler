aac2ba26295adb17068b38ce8fa22179d7040b1a
Kevin Liddle <kevin@8thlight.com>
2013-03-08 15:34:00 -0600

    numba 1

---
+++ 1.hs
@@ -0,0 +1,41 @@
+import Test.Hspec
+
+sumMultiplesFor limit = sumMultiples 0 (limit - 1)
+
+sumMultiples sum limit
+  | limit < 3       = sum
+  | mod limit 3 == 0 = sumMultiples (sum + limit) (limit - 1)
+  | mod limit 5 == 0 = sumMultiples (sum + limit) (limit - 1)
+  | otherwise        = sumMultiples sum (limit - 1)
+
+main = hspec $ do
+  describe "MultipleSummer" $ do
+    it "sums multiples of 3 & 5 up to but not including 3" $ do
+      sumMultiplesFor 3 `shouldBe` 0
+
+    it "sums multiples of 3 & 5 up to but not including 4" $ do
+      sumMultiplesFor 4 `shouldBe` 3
+
+    it "sums multiples of 3 & 5 up to but not including 5" $ do
+      sumMultiplesFor 5 `shouldBe` 3
+
+    it "sums multiples of 3 & 5 up to but not including 6" $ do
+      sumMultiplesFor 6 `shouldBe` 8
+
+    it "sums multiples of 3 & 5 up to but not including 7" $ do
+      sumMultiplesFor 7 `shouldBe` 14
+
+    it "sums multiples of 3 & 5 up to but not including 9" $ do
+      sumMultiplesFor 9 `shouldBe` 14
+
+    it "sums multiples of 3 & 5 up to but not including 10" $ do
+      sumMultiplesFor 10 `shouldBe` 23
+
+    it "sums multiples of 3 & 5 up to but not including 12" $ do
+      sumMultiplesFor 12 `shouldBe` 33
+
+    it "sums multiples of 3 & 5 up to but not including 13" $ do
+      sumMultiplesFor 13 `shouldBe` 45
+
+    it "sums multiples of 3 & 5 up to but not including 1000" $ do
+      sumMultiplesFor 1000 `shouldBe` 233168
